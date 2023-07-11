5116d39fe3183a0796f19d6e5db9ee438b8b8e2e
Tristan Louth-Robins <tristan.louth-robins@csiro.au>
2023-07-11 15:28:32 +0930

    Testing gui apps and new EDA script
    - Python gui for executing programs. Just a test to see if I can get a gui working.
    - Data analysis of AudioMoth deployments in Yankalilla (July 2023)

--- WITA_mf_0723.R
+++ WITA_mf_0723.R
@@ -1,41 +1,72 @@
+library(tidyverse)
+library(rlang)
+library(gghighlight)
 library(ggplot2)
+library(ggrepel)
+library(patchwork)
+library(plotly)
+library(showtext)
 setwd("/Users/tristanlouth-robins/Documents/Documents - MacBook Pro/R data and projects/acoustic_ecology_tests/Wrangling_In_The-Antipodes")
 
 data <- read_csv("data/mf_beach_and_sheoak.csv")
 
-bi_plt <- 
-data %>% 
-  filter(index == "bioacoustic_index",
-         date != "2023-07-01" & date != "2023-07-08") %>% 
-  ggplot() +
-  geom_point(aes(x=time, y=value, colour=site.name), alpha=0.7) +
-  labs(title = "Bioacoustic index",
-       y="Value",
-       x="Time") +
-  facet_wrap(~date, nrow=1)
+# Scatter plot aesthetics and theme --------------------------------------------
+# aesthetics --
+plot.bg <- "#f4f5f2"
+panel.bg <- "#f6f7f5"
+grid.minor <- "#e8ebe6"
+grid.major <- "#e8ebe6"
+leg.bg <- plot.bg
+h.tide <- "#f511e6"
+l.tide <- "#17e0ff"
+points <- "#245757"
+lab1.text <- "#320c85"
+lab1.fill <- "#ffffff"
+lab2.text <- "#000000"
+lab2.fill <- "#afa9ba"
+lab.alpha <- 0.5
+titles.font <- "Helvetica"
+cap.font <- "Avenir Next Condensed Regular"
+lab.font <- "Avenir Next Condensed Regular"
+# theme --
+theme_wita <- function(){
+  theme(
+    text = element_text(family = titles.font, colour = "black"),
+    plot.margin = margin(5,2,5,2, "mm"),
+    plot.background = element_rect(fill = plot.bg, colour = plot.bg),
+    panel.background = element_rect(fill = panel.bg),
+    panel.grid.major = element_line(colour = grid.major, size = 0.5),
+    panel.grid.minor = element_line(colour = grid.major, size = 0.5),
+    legend.title = element_text(family = titles.font, size = 9),
+    legend.text = element_text(family = titles.font, size = 8),
+    legend.background = element_rect(fill = leg.bg),
+    legend.key = element_rect(fill = panel.bg),
+    legend.position = "right",
+  )
+}
 
-aci_plt <- 
+plt_index <- function(index_type, site) {
   data %>% 
-  filter(index == "acoustic_complexity",
-         date != "2023-07-01" & date != "2023-07-08") %>% 
-  ggplot() +
-  geom_point(aes(x=time, y=value, colour=site.name), alpha=0.7, shape = 3) +
-  labs(title = "Acoustic Complexity index",
-       y="Value",
-       x="Time") +
-  facet_wrap(~date, nrow=1)
+    filter(index == index_type,
+           site.name == site,
+           date == "2023-07-03" | date == "2023-07-04") %>% 
+    ggplot() +
+    geom_point(aes(x=time, y=value), colour = points, alpha=0.7) +
+    geom_smooth(aes(x=time, y=value), method="loess", se=F) +
+    facet_wrap(~date, nrow=1) +
+    ylim(0,12)
+}
 
-aci_plt
+plt_aci_bch <-  plt_index("bioacoustic_index", "mf_beach") + 
+  labs(subtitle = "Site 1: 'beach'",
+       caption = "blah blah blah") + theme_wita() 
 
-adi_plt <- 
-  data %>% 
-  filter(index == "acoustic_diversity",
-         date != "2023-07-01" & date != "2023-07-08") %>% 
-  ggplot() +
-  geom_point(aes(x=time, y=value, colour=site.name), alpha=0.7) +
-  labs(title = "Acoustic Diversity index",
-       y="Value",
-       x="Time") +
-  facet_wrap(~date, nrow=1)
+plt_aci_she <-  plt_index("bioacoustic_index", "mf_sheoak") + 
+  labs(subtitle = "Site 2: swamp",
+       caption = "blah blah blah") + theme_wita() 
 
+plt_aci_bch
+plt_aci_she
+
+plt_aci_bch / plt_aci_she
 
